71ffc8aafd427d81ccb49f05fa6f2287cd00b261
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-16 17:29:29 -0700

    Morningzombie

---
+++ src/ifs_ints_and_loops/MorningZombie.java
@@ -0,0 +1,21 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	
+	JOptionPane.showMessageDialog(null, "Brush teeth");
+	JOptionPane.showMessageDialog(null, "Take a shower");
+	JOptionPane.showMessageDialog(null, "Put on clothes");
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+}
